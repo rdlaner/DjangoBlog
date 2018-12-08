71737299803f75d404623f7aba46e9e06cf1b5f3
Dustin L <r.dustin.lane@gmail.com>
2018-12-08 15:21:53 -0800

    Fix settings import in production.py

--- mysite/production.py
+++ mysite/production.py
@@ -1,13 +1,13 @@
 import os
 import dj_database_url
-import settings
+from .settings import *
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///' + os.path.join(settings.BASE_DIR, 'db.sqlite3'))
+    'default': dj_database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
 }
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost']
-STATIC_ROOT = os.path.join(settings.BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ.get('SECRET_KEY')
